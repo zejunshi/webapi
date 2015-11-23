278be5ee8085e71f0cb6e332d57788ffe5e74e8c
Ayushpod <ayush.pod@gmail.com>
2015-11-23 12:02:35 +0100

    first commit

---
+++ Temperature/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Temperature.Startup))]
+namespace Temperature
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
